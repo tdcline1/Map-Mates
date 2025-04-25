01b1afe460687af8f07264104e011b582c76804d
tdcline1 <tdcline3@gmail.com>
2025-04-24 22:54:25 -0400

    add picture to Place compnent and start implementing serializer method

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-
 from .models import Place
 
 
 class PlaceSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(read_only=True, slug_field="username")
+    thumbnail_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Place
-        fields = ("id", "author", "name", "description", "created_at")
+        fields = ("id", "author", "name", "description", "created_at", "thumbnail_url")
+
+    def get_thumbnail_url(self, place):
+        thumbnail = place.get_thumbnail()
+        if thumbnail and thumbnail.image:
+            request = self.context.get("request")
+            return request.build_absolute_uri(thumbnail.image.url)
+        return None
