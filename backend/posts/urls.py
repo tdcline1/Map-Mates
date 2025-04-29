428e8ce90fda0672ee86fd01e0a4762c66997d4f
tdcline1 <tdcline3@gmail.com>
2025-04-29 19:28:07 -0400

    add url for PlaceGeoJSON

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import PlaceList, PlaceDetail
+from .views import PlaceList, PlaceDetail, PlaceGeoJSONView
 
 urlpatterns = [
     path("<int:pk>/", PlaceDetail.as_view(), name="place_detail"),
     path("", PlaceList.as_view(), name="place_list"),
+    path("geojson/", PlaceGeoJSONView.as_view(), name="geojson"),
 ]
