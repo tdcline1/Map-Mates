3c6841c2ee53e642358c7c946763a447d0d2d20c
tdcline1 <tdcline3@gmail.com>
2025-05-01 16:38:59 -0400

    implement preliminary PlaceDetail component

---
+++ frontend/src/components/PlaceDetail.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import api from '../api';
+
+const PlaceDetail = (place_id) => {
+  const [place, setPlace] = useState();
+
+  useEffect(() => {
+    getPlace(place_id);
+  }, []);
+
+  const getPlace = () => {
+    api
+      .get(`api/v1/${place_id}/`)
+      .then((res) => res.data)
+      .then((data) => {
+        setPlace(data);
+      })
+      .catch((err) => alert(err));
+  };
+
+  return (
+    <div>
+      <p>{place.name}</p>
+      <p>{place.subtitle}</p>
+      <p>{place.description}</p>
+      {/* <img src={place.thumbnail_url} width="100%" /> */}
+    </div>
+  );
+};
+
+export default PlaceDetail;
