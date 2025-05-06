58b1a78e0d81745ac8e0936febe7c3ddadebb015
tdcline1 <tdcline3@gmail.com>
2025-05-06 18:09:19 -0400

    add all flyto buttons

--- frontend/src/components/MapControls.jsx
+++ frontend/src/components/MapControls.jsx
@@ -7,14 +7,43 @@ const MapControls = ({ map }) => {
 
   return (
     <>
-      <button
-        className="reset-button"
-        onClick={() => {
-          flyToContinent(22.1194, 51.9013, 2.54);
-        }}
-      >
-        Fly to Europe
-      </button>
+      <div className="map-controls-group map-controls-top-left">
+        <button
+          className="map-control-button"
+          onClick={() => {
+            flyToContinent(18.1194, 50.5013, 3.4);
+          }}
+        >
+          Fly to Europe
+        </button>
+        <button
+          className="map-control-button"
+          onClick={() => {
+            flyToContinent(-97.98846, 35.1129, 3);
+          }}
+        >
+          Fly to North America
+        </button>
+        <button
+          className="map-control-button"
+          onClick={() => {
+            flyToContinent(-64.79085, -21.56653, 2.75);
+          }}
+        >
+          Fly to South America
+        </button>
+        <button
+          className="map-control-button"
+          onClick={() => {
+            flyToContinent(103.26131, 25.45753, 3);
+          }}
+        >
+          Fly to Asia
+        </button>
+      </div>
+      <div className="map-controls-group map-controls-top-right">
+        <button className="map-control-button">Add a Place!</button>
+      </div>
     </>
   );
 };
