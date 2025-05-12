b3869b9967011e7639262d21216cc4e9a3884002
tdcline1 <tdcline3@gmail.com>
2025-05-12 02:02:56 -0400

    fix: change api.js access variable name and lint

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,21 +1,20 @@
-import axios from "axios"
-import { ACCESS_TOKEN } from "./constants"
+import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        return config
-    },
-    (error) => {
-        return Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem('access');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-)
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-export default api
+export default api;
