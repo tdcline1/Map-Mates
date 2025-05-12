3fd9fc7cee408d2925034951646313e38366fbe7
tdcline1 <tdcline3@gmail.com>
2025-05-12 15:04:12 -0400

    feat: add isTokenValid check to api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,21 @@
 import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+const publicEndpoints = ['api/v1/geojson/'];
+
+const isTokenValid = (token) => {
+  try {
+    const decoded = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+    return decoded.exp > currentTime;
+  } catch (err) {
+    return false;
+  }
+};
 
 api.interceptors.request.use(
   (config) => {
