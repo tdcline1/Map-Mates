76a1a16b1d3809ae1f88a0dcf940c748fd3edb53
tdcline1 <tdcline3@gmail.com>
2025-05-14 23:19:58 -0400

    refact: api.js to use AuthContext functions
    enclose interceptors in function so that async, state handling functions can be used(getAccessToken: refresh token and update authstate, logout)

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { STORAGE_KEYS, clearAuthData, isTokenValid } from './utils/auth';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -11,62 +10,59 @@ const isPublicEndpoint = (url) => {
   return publicEndpoints.some((endpoint) => url.includes(endpoint));
 };
 
-api.interceptors.request.use(
-  (config) => {
-    if (isPublicEndpoint(config.url)) {
-      return config;
-    }
-
-    const token = localStorage.getItem(STORAGE_KEYS.ACCESS_TOKEN);
-
-    if (token && isTokenValid(token)) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-// Response interceptor for token refresh
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-
-    if (
-      error.response.status === 401 &&
-      !originalRequest._retry &&
-      !isPublicEndpoint(originalRequest.url)
-    ) {
-      originalRequest._retry = true;
-      const refreshToken = localStorage.getItem(STORAGE_KEYS.REFRESH_TOKEN);
-
-      if (refreshToken) {
-        try {
-          const { data } = await axios.post(
-            `${import.meta.env.VITE_API_URL}/api/token/refresh/`,
-            { refresh: refreshToken }
-          );
-
-          localStorage.setItem(STORAGE_KEYS.ACCESS_TOKEN, data.access);
-          originalRequest.headers.Authorization = `Bearer ${data.access}`;
-          return api(originalRequest);
-        } catch (err) {
-          clearAuthData();
-          window.dispatchEvent(new Event('authStatusChanged'));
-          window.dispatchEvent(new Event('showLoginModal'));
-          return Promise.reject(err);
-        }
-      } else {
-        clearAuthData();
-        window.dispatchEvent(new Event('authStatusChanged'));
-        window.dispatchEvent(new Event('showLoginModal'));
+export const setupApiInterceptors = (getAccessToken, logout) => {
+  // Request interceptor to add token header
+  api.interceptors.request.use(
+    async (config) => {
+      if (isPublicEndpoint(config.url)) {
+        return config;
       }
+
+      const token = await getAccessToken();
+
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
     }
-    return Promise.reject(error);
-  }
-);
+  );
+
+  // Response interceptor for token refresh
+  api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const originalRequest = error.config;
+
+      if (
+        error.response?.status === 401 &&
+        !originalRequest._retry &&
+        !isPublicEndpoint(originalRequest.url)
+      ) {
+        originalRequest._retry = true;
+
+        try {
+          const token = await getAccessToken();
+
+          if (token) {
+            originalRequest.headers.Authorization = `Bearer ${token}`;
+            return api(originalRequest);
+          } else {
+            logout();
+            window.dispatchEvent(new Event('showLoginModal'));
+            return Promise.reject(error);
+          }
+        } catch (refreshError) {
+          logout();
+          window.dispatchEvent(new Event('showLoginModal'));
+          return Promise.reject(refreshError);
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
+};
 
 export default api;
