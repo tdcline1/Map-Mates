f8c0de3384819230b39d02249b6f06ee9bae9d93
tdcline1 <tdcline3@gmail.com>
2025-05-16 22:43:37 -0400

    feat: delete image files when deleting associated place
    Previously the image files remained even though on_delete CASCADE was applied in the model. This change deletes the image files with signal. Does not delete directories though. This will not be necessary on AWS S3s flat file system.

---
+++ backend/posts/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+from .models import PlaceImage
+
+
+@receiver(pre_delete, sender=PlaceImage)
+def delete_place_image_file(sender, instance, **kwargs):
+    if instance.image:
+        instance.image.delete(save=False)
